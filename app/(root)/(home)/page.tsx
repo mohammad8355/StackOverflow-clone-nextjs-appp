4124e87ef06f44e218f82efd2af9947215deab2a
Mohammad <www.almasimohammadmahdi55@gmail.com>
2024-06-15 21:16:06 +0330

    add clerek auth

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,34 @@
 import React from "react";
-
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/nextjs";
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      {" "}
+      <header
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          padding: 20,
+        }}
+      >
+        <h1>My App</h1>
+        <SignedIn>
+          {/* Mount the UserButton component */}
+          <UserButton />
+        </SignedIn>
+        <SignedOut>
+          {/* Signed out users get sign in button */}
+          <SignInButton />
+        </SignedOut>
+      </header>
+    </>
+  );
 };
 
 export default Home;
