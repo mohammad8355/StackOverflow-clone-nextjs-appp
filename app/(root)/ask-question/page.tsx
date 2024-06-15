239760ef323c1903dc700fed3074234b2342f013
Mohammad <www.almasimohammadmahdi55@gmail.com>
2024-06-15 19:11:31 +0330

    base routing of app

---
+++ app/(root)/ask-question/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+import React from "react";
+
+const AskQuestion = () => {
+  return <div>AskQuestion</div>;
+};
+
+export default AskQuestion;
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="light-gradient ">
-      <h1 className="h1-bold primary-text-gradient">
-        DevStack Project Next app
-      </h1>
-    </div>
-  );
-}
