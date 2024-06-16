5723205a6ab9c00c15bd994105f661ffe26a2374
Mohammad <www.almasimohammadmahdi55@gmail.com>
2024-06-16 19:46:45 +0330

    layout change and sign In Form UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,15 +7,41 @@ import {
 } from "@clerk/nextjs";
 import "./globals.css";
 import React from "react";
+import { Metadata } from "next";
+import { Inter, Space_Grotesk } from "next/font/google";
+import "./globals.css";
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+const SpaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+export const metadata: Metadata = {
+  title: "DevFlow | NextJs",
+  description:
+    "An Community of Programmers and Developers that ask questions and answer together ",
+  icons: "/favicon.co",
+};
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body>
+        <body className={`${inter.variable} ${SpaceGrotesk.variable}`}>
           <main>{children}</main>
         </body>
       </html>
